fd18dca60c40a5276c0cf83c89b0f5264f027317
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-02-09 15:06:32 +0200

    Codesandbox example++

--- packages/codesandboxes/signup/src/App.tsx
+++ packages/codesandboxes/signup/src/App.tsx
@@ -4,52 +4,52 @@ import { z } from "zod";
 import { useZorm } from "react-zorm";
 
 const FormSchema = z.object({
-  email: z.string().refine(
-    (val) => {
-      return val.includes("@") && val.includes(".");
-    },
-    { message: "Email must contain a @ sign and a dot" }
-  ),
-  password: z.string().min(8)
+    name: z.string().min(1),
+    age: z
+        .string()
+        .regex(/^[0-9]+$/)
+        .transform(Number),
 });
 
 function ErrorMessage(props: { message: string }) {
-  return <div className="error-message">{props.message}</div>;
+    return <div className="error-message">{props.message}</div>;
 }
 
 export default function Signup() {
-  const zo = useZorm("signup", FormSchema, {
-    onValidSubmit(e) {
-      e.preventDefault();
-      alert("Form ok!\n" + JSON.stringify(e.data, null, 2));
-    }
-  });
-  const disabled = zo.validation?.success === false;
+    const zo = useZorm("signup", FormSchema, {
+        onValidSubmit(e) {
+            e.preventDefault();
+            alert("Form ok!\n" + JSON.stringify(e.data, null, 2));
+        },
+    });
+    const disabled = zo.validation?.success === false;
 
-  return (
-    <form ref={zo.ref}>
-      Email:
-      <input
-        type="text"
-        name={zo.fields.email()}
-        className={zo.errors.email("errored")}
-      />
-      {zo.errors.email((e) => (
-        <ErrorMessage message={e.message} />
-      ))}
-      Password:
-      <input
-        type="password"
-        name={zo.fields.password()}
-        className={zo.errors.password("errored")}
-      />
-      {zo.errors.password((e) => (
-        <ErrorMessage message={e.message} />
-      ))}
-      <button disabled={disabled} type="submit">
-        Signup!
-      </button>
-      <pre>Validation status: {JSON.stringify(zo.validation, null, 2)}</pre>
-    </form>
-  );
+    return (
+        <form ref={zo.ref}>
+            Name:
+            <input
+                type="text"
+                name={zo.fields.name()}
+                className={zo.errors.name("errored")}
+            />
+            {zo.errors.name((e) => (
+                <ErrorMessage message={e.message} />
+            ))}
+            Age
+            <input
+                type="text"
+                name={zo.fields.age()}
+                className={zo.errors.age("errored")}
+            />
+            {zo.errors.age((e) => (
+                <ErrorMessage message="Age must a number" />
+            ))}
+            <button disabled={disabled} type="submit">
+                Signup!
+            </button>
+            <pre>
+                Validation status: {JSON.stringify(zo.validation, null, 2)}
+            </pre>
+        </form>
+    );
 }

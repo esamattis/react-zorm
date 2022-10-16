3fa57b7e346ca175f1bc11b09774eee1633ac140
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-10-16 13:52:23 +0300

    Enable no floating promsies eslint rule

--- packages/react-zorm/.eslintrc.js
+++ packages/react-zorm/.eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint", "react-hooks"],
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
     rules: {
         "react-hooks/rules-of-hooks": "error",
         "react-hooks/exhaustive-deps": "warn",
+        "@typescript-eslint/no-floating-promises": "error",
     },
 };

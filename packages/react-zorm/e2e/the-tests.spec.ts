2cc4379cef9bb25ffa7e12d1842589a7c32d2dd4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-10-17 19:59:24 +0300

    Add formdata event playwright test

--- packages/react-zorm/e2e/the-tests.spec.ts
+++ packages/react-zorm/e2e/the-tests.spec.ts
@@ -21,3 +21,16 @@ test("validates on blur after first submit", async ({ page }) => {
     await page.locator("body").click();
     await expect(error).toBeVisible();
 });
+
+test("can use the formdata inject data", async ({ page }) => {
+    await page.goto("/?test=formdata-event");
+    const error = page.locator(".error");
+    const input = page.locator("input");
+    const validData = page.locator(".valid-data");
+
+    await input.fill("some text");
+    await page.keyboard.press("Enter");
+
+    await expect(validData).toHaveText("formdata: extra data");
+    await expect(error).not.toBeVisible();
+});

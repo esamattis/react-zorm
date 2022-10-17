c3d84590c7abbf3a5e29774fc8dbd05d0d9a828a
Esa-Matti Suuronen <esa-matti@suuronen.org>
2022-10-17 18:47:34 +0300

    List boxes directories with vite

---
+++ packages/codesandboxes/README.md
@@ -0,0 +1,18 @@
+# Codesandbox demos
+
+## Run locally
+
+```
+pnpm i
+pnpm run dev
+```
+
+And open the logged url
+
+## Updating box deps
+
+See and update `merge-deps.mjs` and run
+
+```
+pnpm run upgrade-box-deps
+```
--- packages/codesandboxes/vite.config.ts
+++ packages/codesandboxes/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { directoryPlugin } from "vite-plugin-list-directory-contents";
+
+const ROOT = __dirname + "/boxes";
 
 export default defineConfig({
-    root: __dirname + "/boxes",
-    plugins: [react()],
+    root: ROOT,
+    plugins: [react(), directoryPlugin({ baseDir: ROOT })],
 });

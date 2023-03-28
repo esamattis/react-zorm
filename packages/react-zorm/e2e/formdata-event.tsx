fa277b554fa7fcd0f364f2bb0895bf1dbf2ce61d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-03-28 13:16:37 +0300

    Test stale closures in onFormdata

--- packages/react-zorm/e2e/formdata-event.tsx
+++ packages/react-zorm/e2e/formdata-event.tsx
@@ -12,19 +12,31 @@ function Test() {
     const [validFormData, setValidFormData] =
         useState<z.infer<typeof Schema>>();
 
+    // use state to detect stale closure in onFormData
+    const [extra, setExtra] = useState("");
+
     const zo = useZorm("form", Schema, {
         onValidSubmit(e) {
             e.preventDefault();
             setValidFormData(e.data);
         },
         onFormData(e) {
-            e.formData.set(zo.fields.extra(), "extra data");
+            e.formData.set(zo.fields.extra(), extra);
         },
     });
 
     return (
         <form ref={zo.ref} data-testid="form">
             <input data-testid="input" name={zo.fields.input()} />
+
+            <button
+                type="button"
+                onClick={() => {
+                    setExtra("extra data");
+                }}
+            >
+                set extra
+            </button>
 
             <button>submit with extra data</button>
 

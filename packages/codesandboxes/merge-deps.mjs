5c1d17bcc3b7bbfc52fc1126a12d7f25a9d50a38
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-03-28 14:15:14 +0300

    Upgrade codesandboxes to rz v0.8.0

--- packages/codesandboxes/merge-deps.mjs
+++ packages/codesandboxes/merge-deps.mjs
@@ -8,7 +8,7 @@ Object.assign(pkg.dependencies, {
     "@types/react-dom": "18.0.6",
     react: "18.2.0",
     "react-dom": "18.2.0",
-    "react-zorm": "0.7.0-dev.aed1320198",
+    "react-zorm": "0.8.0",
     zod: "3.21.4",
 });
 

3129f39c9c82463d22e28f738cdffad35e6494d4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2023-03-28 03:52:39 +0300

    fixup

--- packages/react-zorm/__tests__/field-type-inspection.test.ts
+++ packages/react-zorm/__tests__/field-type-inspection.test.ts
@@ -146,7 +146,7 @@ test("can access the input name", () => {
     expect(chain.arr(0).deep((field) => field.name)).toEqual("arr[0].deep");
 });
 
-test.skip("[type only] can resturn the from the chain", () => {
+test.skip("[type only] can return the from the chain", () => {
     const Schema = z.object({
         field: z.string(),
     });
